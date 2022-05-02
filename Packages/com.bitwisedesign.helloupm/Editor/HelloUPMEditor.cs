4410d6741a453720c864da8b1f55f1b9c3d70126
Ray Graham <rayg@bitwisedesign.com>
2022-05-02 08:26:08 -0700

    First commit

---
+++ Packages/com.bitwisedesign.helloupm/Editor/HelloUPMEditor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using BitwiseDesign;
+
+[CustomEditor(typeof(HelloUPM))]
+public class HelloUPMEditor : Editor
+{
+
+}
---
+++ Packages/com.bitwisedesign.helloupm/Runtime/HelloUPM.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BitwiseDesign
+{
+    public class HelloUPM : MonoBehaviour
+    {
+        public string awakeText = "Hello UPM!";
+
+        void Awake()
+        {
+            Debug.Log(awakeText);
+        }
+    }
+}
